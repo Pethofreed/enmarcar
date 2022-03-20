f80174156bafbd5cfff6edffe5b91c8e4f4f05a3
Carlos Valencia <rasfreed@gmail.com>
2022-03-19 20:40:29 -0500

    se agrega logo real

--- src/Components/Login/index.jsx
+++ src/Components/Login/index.jsx
@@ -1,5 +1,6 @@
 import './styles.css';
 import Card from '@mui/material/Card';
+import Logo from './logo_enmarcar.png';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
@@ -9,17 +10,12 @@ import CardContent from '@mui/material/CardContent';
 const Login = () => {
   return(
     <div className="login-container">
-      <Typography variant="h2" component="div" mb={5} sx={{ color: '#eee' }}>
-        Enmarcar
-      </Typography>
       <Card sx={{ minWidth: 275 }}>
         <CardContent>
-          <Typography variant="h5" component="div" mb={2} align="center">
-            Iniciar Sesion
-          </Typography>
+          <img src={Logo} alt="Enmarcar logo" className='logo-enmarcar'/>
           <div className="form-inputs">
             <TextField id="standard-basic" label="Usuario" variant="standard" />
-            <TextField id="standard-basic" label="Contraseña" variant="standard" />
+            <TextField id="standard-basic" label="Contraseña" variant="standard" type="password"/>
           </div>
 
         </CardContent>
