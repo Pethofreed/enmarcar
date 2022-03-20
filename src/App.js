cd8dd3f9439dc83940866cd22801a5d3f67be1d1
Carlos Valencia <rasfreed@gmail.com>
2022-03-19 21:44:07 -0500

    se agrega navegacion react-router-dom y primer user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
+import { Router,
+  Route,
+  Redirect,
+  Switch
+  } from 'react-router-dom';
 import Login from './Components/Login';
+import { history } from './Utils/history';
+
+function PrivateRoute({children, ...rest}) {
+  const token = localStorage.getItem('token')
+  return (
+    <Route {...rest} render={() => {
+      return token ? children : <Redirect to="/signin" />
+    }} />
+  )
+}
 
 function App() {
   return (
-    <Login />
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <PrivateRoute exact path="/dashboard">
+
+        </PrivateRoute>
+      </Switch>
+    </Router>
   );
 }
 
--- src/Components/Login/index.jsx
+++ src/Components/Login/index.jsx
@@ -1,26 +1,61 @@
 import './styles.css';
+import { useState } from 'react';
+import { history } from '../../Utils/history';
 import Card from '@mui/material/Card';
 import Logo from './logo_enmarcar.png';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 
+const users = [
+  { username: 'admin', pass: 'root' },
+  { username: 'Usuario_uno', pass: 'root' },
+]
+
 const Login = () => {
+
+  const [user, setUser] = useState('');
+  const [error, setError] = useState(false);
+  const [password, setPassword] = useState('');
+
+  const handleSession = () => {
+    users.forEach(({username , pass}) => {
+      if (user === username && password === pass) {
+        setError(false);
+        setPassword('');
+        setUser('');
+        history.push('/dashboard')
+      } else setError(true);
+    })
+  }
+
   return(
     <div className="login-container">
       <Card sx={{ minWidth: 275 }}>
         <CardContent>
           <img src={Logo} alt="Enmarcar logo" className='logo-enmarcar'/>
           <div className="form-inputs">
-            <TextField id="standard-basic" label="Usuario" variant="standard" />
-            <TextField id="standard-basic" label="Contraseña" variant="standard" type="password"/>
-          </div>
+            <TextField
+              id="standard-basic"
+              label="Usuario"
+              variant="standard"
+              onChange={(e) => setUser(e.target.value)}
+              value={user}
+            />
 
+            <TextField
+              id="standard-basic"
+              label="Contraseña"
+              variant="standard"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </div>
         </CardContent>
         <CardActions>
-          <Button size="small">Ingresar</Button>
+          <Button size="small" onClick={() => handleSession()}>Ingresar</Button>
         </CardActions>
       </Card>
     </div>
